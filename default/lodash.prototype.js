06a2504bf4ee19dcc179b90ad75d57eaabd754b4
Falk Köppe <falk.koeppe@gmail.com>
2020-12-02 17:41:00 +0100

    reset

--- default/lodash.prototype.js
+++
@@ -1,8 +0,0 @@
-function clamp(number, lower, upper) {
-  number = number <= upper ? number : upper;
-  number = number >= lower ? number : lower;
-
-  return number;
-};
-
-_.mixin({ 'clamp': clamp });
--- default/source.prototype.js
+++
@@ -1,8 +0,0 @@
-Source.prototype.harvestSlots = function() {
-  const terrain = this.room.getTerrain();
-  const adjacentPositions = this.pos.adjacentPositions();
-
-  return _.filter(adjacentPositions, function(position) {
-    return terrain.get(position.x, position.y) != TERRAIN_MASK_WALL;
-  }).length;
-};
