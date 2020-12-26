de4fe4efce22bb9978c6bf31fe30fc89efe63f02
Falk Köppe <falk.koeppe@gmail.com>
2020-12-26 22:36:54 +0100

    init and turn off everything to get it to work

--- src/main.js
+++ src/main.js
@@ -5,23 +5,23 @@ require('require')
 globalManager.notifyVersionChange()
 
 module.exports.loop = function() {
-  statsManager.reset()
-  World.clean()
-  World.update()
+  // statsManager.reset()
+  // World.clean()
+  // World.update()
 
   _.forEach(Game.rooms, function(room) {
-    roomManager.visuals(room)
+    // roomManager.visuals(room)
 
     if(room.my) {
-      roomManager.defense(room)
-      creepManager.spawn(room)
-      creepManager.run()
+      // roomManager.defense(room)
+      // creepManager.spawn(room)
+      // creepManager.run()
 
-      statsManager.exportRoomStats(room)
+      // statsManager.exportRoomStats(room)
     }
   })
 
-  statsManager.exportGlobalStats()
+  // statsManager.exportGlobalStats()
 
   Sandbox.run()
 }
